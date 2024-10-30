51f9fee106a4bd2e61923a20d56b814df53ee855
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2024-10-30 08:22:47 +0100

    Create Ngrx Task's actions.

--- src/app/features/tasks/store/actions/tasks.actions.ts
+++ src/app/features/tasks/store/actions/tasks.actions.ts
@@ -1,10 +1,18 @@
-import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
+import { Task } from '../../models/task.model';
 
-export const TasksActions = createActionGroup({
-  source: 'Tasks',
-  events: {
-    'Load Taskss': emptyProps(),
-    
-    
-  }
-});
+export const loadTasks = createAction('[Tasks] Load Tasks');
+export const loadTasksSuccess = createAction('[Tasks API] Load Tasks Success', props<{ tasks: Task[] }>());
+export const loadTasksFailure = createAction('[Tasks API] Load Tasks Failure', props<{ error: any }>());
+
+export const createTask = createAction('[Tasks Form] Create Task', props<{ task: Task }>());
+export const createTaskSuccess = createAction('[Tasks API] Create Task Success', props<{ task: Task }>());
+export const createTaskFailure = createAction('[Tasks API] Create Task Failure', props<{ error: any }>());
+
+export const updateTask = createAction('[Tasks Detail] Update Task', props<{ task: Task }>());
+export const updateTaskSuccess = createAction('[Tasks API] Update Task Success', props<{ task: Task }>());
+export const updateTaskFailure = createAction('[Tasks API] Update Task Failure', props<{ error: any }>());
+
+export const deleteTask = createAction('[Tasks Detail] Delete Task', props<{ task: Task }>());
+export const deleteTaskSuccess = createAction('[Tasks API] Delete Task Success', props<{ task: Task }>());
+export const deleteTaskFailure = createAction('[Tasks API] Delete Task Failure', props<{ error: any }>());
