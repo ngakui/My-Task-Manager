07cc04db2bfde2b5be3aad95d88683363862f76d
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2024-10-31 16:47:46 +0100

    Implement reducer tests.

--- src/app/features/tasks/store/reducers/tasks.reducer.spec.ts
+++ src/app/features/tasks/store/reducers/tasks.reducer.spec.ts
@@ -1,13 +1,24 @@
-import { reducer, initialState } from './tasks.reducer';
+import { tasksReducer, initialState } from './tasks.reducer';
+import * as TasksActions  from '../actions/tasks.actions';
 
 describe('Tasks Reducer', () => {
   describe('an unknown action', () => {
     it('should return the previous state', () => {
       const action = {} as any;
 
-      const result = reducer(initialState, action);
+      const result = tasksReducer(initialState, action);
 
       expect(result).toBe(initialState);
+    });
+  });
+
+  describe('loadTasks', () => {
+    it('should set loading to true', () => {
+      const action = TasksActions.loadTasks();
+
+      const result = tasksReducer(initialState, action);
+
+      expect(result.loading).toBeTrue();
     });
   });
 });
