55cbdf02cfe21884e8e3fe142daa71a6855aa2e6
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2024-10-31 16:48:46 +0100

    Create Ngrx Tasks Effects.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,8 @@ import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideRouterStore } from '@ngrx/router-store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }), provideRouterStore()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }), provideRouterStore(), provideEffects()]
 };
