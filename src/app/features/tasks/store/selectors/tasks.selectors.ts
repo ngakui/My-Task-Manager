6010ca8ffd35865ed87f1e5ceb1973c16db485eb
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2024-10-31 16:48:14 +0100

    Create Ngrx Tasks Selectors.

--- src/app/features/tasks/store/selectors/tasks.selectors.ts
+++ src/app/features/tasks/store/selectors/tasks.selectors.ts
@@ -1,2 +1,24 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { TasksState, selectAll } from '../reducers/tasks.reducer';
 
+export const selectTasksState = createFeatureSelector<TasksState>('tasks');
+
+export const selectAllTasks = createSelector(
+  selectTasksState,
+  selectAll
+);
+
+export const selectLoading = createSelector(
+  selectTasksState,
+  (state: TasksState) => state.loading
+);
+
+export const selectError = createSelector(
+  selectTasksState,
+  (state: TasksState) => state.error
+);
+
+export const selectTaskById = (taskId: string) => createSelector(
+  selectTasksState,
+  state => state.entities[taskId]
+);
