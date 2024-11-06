57019ff547418cee8ee4a113dafe42d761ccfbad
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2024-11-06 19:35:53 +0100

    Update app config with Task reducer and effects.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,23 @@ import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideRouterStore } from '@ngrx/router-store';
 import { provideEffects } from '@ngrx/effects';
+import { tasksFeatureKey, tasksReducer } from './features/tasks/store/reducers/tasks.reducer';
+import { TasksEffects } from './features/tasks/store/effects/tasks.effects';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }), provideRouterStore(), provideEffects()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), 
+    provideHttpClient(),
+    provideStore({[tasksFeatureKey]: tasksReducer}),
+    provideEffects([TasksEffects]),
+    provideStoreDevtools({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+      connectInZone: true // If set to true, the connection is established within the Angular zone
+    }), 
+    provideRouterStore(),
+  ]
 };
