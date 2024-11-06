f3b439e2e1140a7694a8cfe746e1c605f9fe0306
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2024-11-06 19:42:00 +0100

    Add Store feature and effect feature.

--- src/app/features/tasks/tasks.module.ts
+++ src/app/features/tasks/tasks.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TasksRoutingModule } from './tasks-routing.module';
+import { SharedModule } from '../../shared/shared.module';
+import { provideStore, StoreModule } from '@ngrx/store';
+import { tasksFeatureKey, tasksReducer } from './store/reducers/tasks.reducer';
+import { EffectsModule, provideEffects } from '@ngrx/effects';
+import { TasksEffects } from './store/effects/tasks.effects';
 
 
 @NgModule({
+  providers: [
+    provideStore({[tasksFeatureKey]: tasksReducer}),
+    provideEffects(TasksEffects)
+  ],
   declarations: [],
   imports: [
     CommonModule,
-    TasksRoutingModule
+    TasksRoutingModule,
+    SharedModule,
+    StoreModule.forFeature(tasksFeatureKey, tasksReducer),
+    EffectsModule.forFeature([TasksEffects])
   ]
 })
 export class TasksModule { }
