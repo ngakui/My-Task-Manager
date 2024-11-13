36ad16bb88d1225daf0f3fe6472cd1f262e6055b
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2024-11-13 15:41:03 +0100

    Add Load One Task in Actions.

--- src/app/features/tasks/store/actions/tasks.actions.ts
+++ src/app/features/tasks/store/actions/tasks.actions.ts
@@ -5,6 +5,11 @@ export const loadTasks = createAction('[Tasks] Load Tasks');
 export const loadTasksSuccess = createAction('[Tasks API] Load Tasks Success', props<{ tasks: Task[] }>());
 export const loadTasksFailure = createAction('[Tasks API] Load Tasks Failure', props<{ error: any }>());
 
+export const loadOneTasks = createAction('[Task] Load Task', props<{ id: string }>());
+export const loadOneTaskSuccess = createAction('[Task API] Load Task Success', props<{ task: Task[] }>());
+export const loadOneTaskFailure = createAction('[Task API] Load Task Failure', props<{ error: any }>());
+
+
 export const createTask = createAction('[Tasks Form] Create Task', props<{ task: Task }>());
 export const createTaskSuccess = createAction('[Tasks API] Create Task Success', props<{ task: Task }>());
 export const createTaskFailure = createAction('[Tasks API] Create Task Failure', props<{ error: any }>());
