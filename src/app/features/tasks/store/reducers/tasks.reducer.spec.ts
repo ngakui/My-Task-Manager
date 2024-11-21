dc0ecd003713f75b4eb3845e4320ef3bde3ea4be
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2024-11-21 19:07:16 +0100

    Create test for reducer.

--- src/app/features/tasks/store/reducers/tasks.reducer.spec.ts
+++ src/app/features/tasks/store/reducers/tasks.reducer.spec.ts
@@ -1,5 +1,6 @@
 import { tasksReducer, initialState } from './tasks.reducer';
 import * as TasksActions  from '../actions/tasks.actions';
+import { Task, TaskStatus } from '../../models/task.model';
 
 describe('Tasks Reducer', () => {
   describe('an unknown action', () => {
@@ -19,6 +20,146 @@ describe('Tasks Reducer', () => {
       const result = tasksReducer(initialState, action);
 
       expect(result.loading).toBeTrue();
+    });
+  });
+
+  describe('loadTasksSuccess', () => {
+    it('should set loading to false and add tasks', () => {
+
+      const tasks: Task[] = [
+        {id: '1', title: 'Task 1', description: 'Description 1', creationDate: new Date(), dueDate: new Date(), completed: false, status: TaskStatus.Todo},
+        {id: '2', title: 'Task 2', description: 'Description 2', creationDate: new Date(), dueDate: new Date(), completed: false, status: TaskStatus.Todo},
+      ];
+
+      const action = TasksActions.loadTasksSuccess({ tasks });
+
+      const result = tasksReducer({ ...initialState, loading: true }, action);
+
+      expect(result.loading).toBeFalse();
+      expect(result.entities['1']).toEqual(tasks[0]);
+    });
+  });
+
+  describe('loadOneTask', () => {
+    it('should set loading to true', () => {
+      const action = TasksActions.loadOneTask({ id: '1' });
+
+      const result = tasksReducer(initialState, action);
+
+      expect(result.loading).toBeTrue();
+    });
+  });
+
+  describe('loadOneTaskSuccess', () => {
+    it('should set loading to false and add task', () => {
+
+      const task: Task = {id: '1', title: 'Task 1', description: 'Description 1', creationDate: new Date(), dueDate: new Date(), completed: false, status: TaskStatus.Todo};
+
+      const action = TasksActions.loadOneTaskSuccess({ task });
+
+      const result = tasksReducer({ ...initialState, loading: true }, action);
+
+      expect(result.loading).toBeFalse();
+      expect(result.entities['1']).toEqual(task);
+    });
+  });
+
+  describe('createTaskSuccess', () => {
+    it('should add task', () => {
+
+      const task: Task = {id: '1', title: 'Task 1', description: 'Description 1', creationDate: new Date(), dueDate: new Date(), completed: false, status: TaskStatus.Todo};
+
+      const action = TasksActions.createTaskSuccess({ task });
+
+      const result = tasksReducer(initialState, action);
+
+      expect(result.entities['1']).toEqual(task);
+    });
+  });
+
+  describe('updateTaskSuccess', () => {
+    it('should update task', () => {
+
+      const task: Task = {id: '1', title: 'Task 1', description: 'Description 1', creationDate: new Date(), dueDate: new Date(), completed: false, status: TaskStatus.Todo};
+
+      const action = TasksActions.updateTaskSuccess({ task });
+
+      const result = tasksReducer({ ...initialState, entities: { '1': task } }, action);
+
+      expect(result.entities['1']).toEqual(task);
+    });
+  });
+
+  describe('deleteTaskSuccess', () => {
+    it('should delete task', () => {
+
+      const task: Task = {id: '1', title: 'Task 1', description: 'Description 1', creationDate: new Date(), dueDate: new Date(), completed: false, status: TaskStatus.Todo};
+
+      const action = TasksActions.deleteTaskSuccess({ task });
+
+      const result = tasksReducer({ ...initialState, entities: { '1': task } }, action);
+
+      expect(result.entities['1']).toBeUndefined();
+    });
+  });
+
+  describe('loadTasksFailure', () => {
+    it('should set error', () => {
+      const error = 'Error loading tasks';
+
+      const action = TasksActions.loadTasksFailure({ error });
+
+      const result = tasksReducer(initialState, action);
+
+      expect(result.error).toEqual(error);
+    });
+  });
+
+  describe('loadOneTaskFailure', () => {
+    it('should set error', () => {
+      const error = 'Error loading task';
+
+      const action = TasksActions.loadOneTaskFailure({ error });
+
+      const result = tasksReducer(initialState, action);
+
+      expect(result.error).toEqual(error);
+    });
+  });
+
+  describe('createTaskFailure', () => {
+    it('should set error', () => {
+      const error = 'Error creating task';
+
+      const action = TasksActions.createTaskFailure({ error });
+
+      const result = tasksReducer(initialState, action);
+
+      expect(result.error).toEqual(error);
+    });
+  });
+
+  describe('updateTaskFailure', () => {
+    it('should set error', () => {
+      const error = 'Error updating task';
+
+      const action = TasksActions.updateTaskFailure({ error });
+
+      const result = tasksReducer(initialState, action);
+
+      expect(result.error).toEqual(error);
+    });
+  });
+
+  describe('deleteTaskFailure', () => {
+    it('should set error', () => {
+      const error = 'Error deleting task';
+
+      const action = TasksActions.deleteTaskFailure({ error });
+
+      const result = tasksReducer(initialState, action);
+
+      expect(result.error).toEqual(error);
     });
   });
 });
