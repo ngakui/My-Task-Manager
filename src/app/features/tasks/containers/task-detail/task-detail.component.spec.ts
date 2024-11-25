486279934c8bf46ed96cd035e90015cf50dc5a01
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2024-11-25 21:43:37 +0100

    Update task detail tests.

--- src/app/features/tasks/containers/task-detail/task-detail.component.spec.ts
+++ src/app/features/tasks/containers/task-detail/task-detail.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
 import { TaskDetailComponent } from './task-detail.component';
+import { provideMockStore } from '@ngrx/store/testing';
 
 describe('TaskDetailComponent', () => {
   let component: TaskDetailComponent;
   let fixture: ComponentFixture<TaskDetailComponent>;
+  const fakeActivatedRoute = {
+    snapshot: { data: { } }
+  } as ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TaskDetailComponent]
+      imports: [TaskDetailComponent],
+      providers: [{provide: ActivatedRoute, useValue: fakeActivatedRoute}, provideMockStore({})]
     })
     .compileComponents();
 
