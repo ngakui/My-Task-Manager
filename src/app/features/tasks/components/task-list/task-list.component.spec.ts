27c8034a68388bd41ece7e1509f5e7faa09b32ba
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2024-11-25 21:44:05 +0100

    Update task list test.

--- src/app/features/tasks/components/task-list/task-list.component.spec.ts
+++ src/app/features/tasks/components/task-list/task-list.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideMockStore } from '@ngrx/store/testing';
 import { TaskListComponent } from './task-list.component';
+import { StateObservable, Store } from '@ngrx/store';
+import { ActivatedRoute } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 describe('TaskListComponent', () => {
   let component: TaskListComponent;
   let fixture: ComponentFixture<TaskListComponent>;
+  const fakeActivatedRoute = {
+    snapshot: { data: { } }
+  } as ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TaskListComponent]
+      imports: [TaskListComponent],
+      providers: [provideMockStore({}), { provide: ActivatedRoute, useValue: fakeActivatedRoute }, provideAnimationsAsync()]
     })
     .compileComponents();
 
